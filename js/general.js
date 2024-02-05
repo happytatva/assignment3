9d6d875672dfc96305601ff00ff7475a2f316b83
happytatva <happy.patel@etatvasoft.com>
2024-02-05 13:54:16 +0530

    add comment in js

--- js/general.js
+++ js/general.js
@@ -3,54 +3,55 @@ $(document).ready(function () {
     $(".body").toggleClass("open-menu");
   });
 });
-
+// -------sliding logogs using slick----------
 $(document).ready(function () {
-     $(".logo-slider").slick({
-          infinite: false,
-          slidesToShow: 5,
-          slidesToScroll: 2,
-          responsive: [
-            {
-              breakpoint: 991,
-              settings: {
-                slidesToShow: 4,
-              },
-            },
-            {
-              breakpoint: 767,
-              settings: {
-                slidesToShow: 3,
-              },
-            },
-            {
-              breakpoint: 567,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              },
-            },
-            {
-              breakpoint: 365,
-              settings: {
-                slidesToShow: 1,
-              },
-            },
-          ],
-     });
-        
+  $(".logo-slider").slick({
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 567,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 365,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  //   ----setting top value of open menu---------
   if ($(window).width() < 1025) {
     var headerWrapHeight1 = $(".header-wrap").height();
     $(".body .header-inner").css("top", headerWrapHeight1);
   }
 });
-  $(window).on("resize", function () {
-     if ($(window).width() < 1025) {
-     // Get the height + padding + border of `header-wrap`
-     setTimeout(function(){
-          var headerWrapHeight = $(".header-wrap").outerHeight();
-          $(".body .header-inner").css("top", headerWrapHeight);
-     },500)
-     
-     console.log("resize")
-     }
-  });  
+// ------setting top value of open menu while resizing--------
+$(window).on("resize", function () {
+  if ($(window).width() < 1025) {
+    // Get the height + padding + border of `header-wrap`
+    setTimeout(function () {
+      var headerWrapHeight = $(".header-wrap").outerHeight();
+      $(".body .header-inner").css("top", headerWrapHeight);
+    }, 500);
+
+    console.log("resize");
+  }
+});
