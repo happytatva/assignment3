4ce1503232cb3c6a1baff0e0e1be079424dd8330
happytatva <happy.patel@etatvasoft.com>
2024-02-05 13:45:21 +0530

    modified hamburger menu

--- js/general.js
+++ js/general.js
@@ -1,44 +1,56 @@
-// Look for .hamburger
-var hamburger = document.querySelector(".hamburger");
-var headerInner = document.getElementById("header-inner");
-hamburger.addEventListener("click", function () {
-  // Toggle class "is-active"
-  hamburger.classList.toggle("is-active");
-  headerInner.classList.toggle("show-nav");
-});
-
-//   --for slick slider---
 $(document).ready(function () {
-  $(".logo-slider").slick({
-    infinite: false,
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 4,
-        },
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 567,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-          breakpoint: 365,
-          settings: {
-            slidesToShow: 1
-          }
-        },
-    ],
+  $(".hamburger").click(function () {
+    $(".body").toggleClass("open-menu");
   });
 });
+
+$(document).ready(function () {
+     $(".logo-slider").slick({
+          infinite: false,
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 4,
+              },
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 567,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              },
+            },
+            {
+              breakpoint: 365,
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ],
+     });
+        
+  if ($(window).width() < 1025) {
+    var headerWrapHeight1 = $(".header-wrap").height();
+    $(".body .header-inner").css("top", headerWrapHeight1);
+  }
+});
+  $(window).on("resize", function () {
+     if ($(window).width() < 1025) {
+     // Get the height + padding + border of `header-wrap`
+     setTimeout(function(){
+          var headerWrapHeight = $(".header-wrap").outerHeight();
+          $(".body .header-inner").css("top", headerWrapHeight);
+     },500)
+     
+     console.log("resize")
+     }
+  });  
